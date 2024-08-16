01099d5d9fb2fd8bab57e375162bf4da6340b7ef
theactualdev <theolayinkayodele@gmail.com>
2024-08-16 18:22:32 +0100

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu');
+const collapse = document.querySelector('.collapse');
+
+menuToggle.addEventListener('click', () => {
+    collapse.classList.toggle('active');
+})
