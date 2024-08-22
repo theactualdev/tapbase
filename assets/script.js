6d6a2bae707b2d21143f3356354b5799de56ca09
Peter Griffin <thecouchpeter@gmail.com>
2024-08-22 19:14:00 +0100

    udat

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+    const menuToggle = document.querySelector('.menu');
+const collapse = document.querySelector('.collapse');
+
+menuToggle.addEventListener('click', () => {
+    collapse.classList.toggle('active');
+})
